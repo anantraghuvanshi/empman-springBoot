d8f53cefdc071f6fe25b61c5777e446c43a42501
Anant <anantsinghraghuvanshi1@gmail.com>
2023-05-19 13:57:52 +0530

    initial commit

---
+++ src/test/java/com/anant/empmanager/EmpmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anant.empmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
