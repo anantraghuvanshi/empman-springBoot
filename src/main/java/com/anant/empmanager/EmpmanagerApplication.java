254def22064c9d5c29a9bdae00952621d14cb7d7
Anant <anantsinghraghuvanshi1@gmail.com>
2023-05-19 16:18:02 +0530

    database not connecting

--- src/main/java/com/anant/empmanager/EmpmanagerApplication.java
+++ src/main/java/com/anant/empmanager/EmpmanagerApplication.java
@@ -2,8 +2,9 @@ package com.anant.empmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EmpmanagerApplication {
 
 	public static void main(String[] args) {
